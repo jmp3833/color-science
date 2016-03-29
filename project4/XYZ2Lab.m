b6752801168f8972e91c389e78727e2a3a9b26d3
jmp3833 <jmp3833@rit.edu>
2016-03-29 12:18:48 -0400

    Optimize DeltaEAB and LAB function

--- project4/XYZ2Lab.m
+++ project4/XYZ2Lab.m
@@ -5,28 +5,19 @@
 % CIElab values based on these tristimulus values.
 function Lab = XYZ2Lab(XYZ, XYZn)
 
-  sz = size(XYZ);
-  numCols = sz(2);
-  
-  for col = 1:(numCols)
-      
-      XYZind = XYZ(:,col);
-
       % Calculate function result values that will be required to calculate Lab
-      funcXXn = calcLabFuncResult(XYZind(1) / XYZn(1));
-      funcYYn = calcLabFuncResult(XYZind(2) / XYZn(2));
-      funcZZn = calcLabFuncResult(XYZind(3) / XYZn(3));
+      funcXXn = calcLabFuncResult(XYZ(1, :) / XYZn(1));
+      funcYYn = calcLabFuncResult(XYZ(2, :) / XYZn(2));
+      funcZZn = calcLabFuncResult(XYZ(3, :) / XYZn(3));
 
       % Calculate individual Lab values
       L = 116 * funcYYn - 16;
       a = 500 * (funcXXn - funcYYn);
       b = 200 * (funcYYn - funcZZn);
       
-      labInd = [L, a, b];
-
-      % Append lab values to 3 x 1 array
-      Lab(:,col) = labInd;
-  end
+      Lab = [L;a;b];
+      
+      
 end
 
 % Calculates the inner value of f(x) in the CIELab algorithm
@@ -35,9 +26,8 @@ end
 %if x > .008856, calc x^(1/3)
 %otherwise calc 7.787x + 16/116
 function fResult = calcLabFuncResult(x)
-  if x > .008856
-    fResult = x ^ (1/3);   
-  else
-    fResult = 7.787 * x + (16/116);
-  end
+  x(x > .008856) = x(x > .008856).^(1/3);
+  x(x <= .008856) = x(x <= .008856) * 7.787 + (16/116);
+  
+  fResult = x;
 end
--- project4/deltaEab.m
+++ project4/deltaEab.m
@@ -1,15 +1,6 @@
 %Calculate delta values between two sets of Lab values
 %This function operates on a 3 x n column matrix
 function DEab = deltaEab (Lab1, Lab2)
-
-  sz = size(Lab1);
-  numCols = sz(2);
-  
-  for col = 1:(numCols) 
-    l1 = Lab1(:,col);
-    l2 = Lab2(:,col);
-    DEab(col) = sqrt((l2(1) - l1(1))^2 + (l2(2) - l1(2))^2 ...
-    + (l2(3) - l1(3))^2);
-  end
-  
+    DEab = sqrt((Lab2(1,:) - Lab1(1,:)).^2 + (Lab2(2,:) - Lab1(2,:)).^2 ...
+    + (Lab2(3,:) - Lab1(3,:)).^2);
 end
