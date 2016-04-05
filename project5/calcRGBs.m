2ece4a6f07d52af6a56e60463b5f0c0447352141
jmp3833 <jmp3833@rit.edu>
2016-04-05 11:03:18 -0400

    Complete up to step 10 of lab

--- project5/calcRGBs.m
+++ project5/calcRGBs.m
@@ -15,10 +15,10 @@ disp(cam_RGBs);
 
 %Grab final 'row' of RGB values for colorchecer chart
 %Sort from black to white
-cam_grey_rgbs = cam_RGBs(:,19:24);
-cam_grey_rgbs = fliplr(cam_grey_rgbs);
-fprintf('cam_grey_rgbs = \n');
-disp(cam_grey_rgbs);
+cam_gray_rgbs = cam_RGBs(:,19:24);
+cam_gray_rgbs = fliplr(cam_gray_rgbs);
+fprintf('cam_gray_rgbs = \n');
+disp(cam_gray_rgbs);
 
 %% Lab Step 4 - Calculate normalized Y values
 %Calculate normalized Y values for patches 19-24 
@@ -32,11 +32,86 @@ disp(munki_gray_Ys);
 %% Graph RGB Versus Grey Y values
 clf;
 hold on;
-plot(munki_gray_Ys, cam_grey_rgbs(1,:));
-plot(munki_gray_Ys, cam_grey_rgbs(2,:));
-plot(munki_gray_Ys, cam_grey_rgbs(3,:));
+plot(munki_gray_Ys, cam_gray_rgbs(1,:));
+plot(munki_gray_Ys, cam_gray_rgbs(2,:));
+plot(munki_gray_Ys, cam_gray_rgbs(3,:));
 title('original greyscale Y to RGB relationship');
 xlabel('munki gray Ys');
 ylabel('camera grey RGBs');
-
 hold off;
+
+%% Linearize Camera RGB Output and Plot
+
+r = 1; g = 2; b = 3;
+% fit low-order polynomial functions between the camera gray rgbs
+% and the munki gray Ys
+cam_polys(r,:)=polyfit(cam_gray_rgbs(r,:),munki_gray_Ys,3);
+cam_polys(g,:)=polyfit(cam_gray_rgbs(g,:),munki_gray_Ys,3);
+cam_polys(b,:)=polyfit(cam_gray_rgbs(b,:),munki_gray_Ys,3);
+
+% use the functions to linearize the camera RGB data
+cam_rgbs_lin(r,:) = polyval(cam_polys(r,:),cam_RGBs(r,:));
+cam_rgbs_lin(g,:) = polyval(cam_polys(g,:),cam_RGBs(g,:));
+cam_rgbs_lin(b,:) = polyval(cam_polys(b,:),cam_RGBs(b,:));
+
+% clip out of range values
+cam_rgbs_lin(cam_rgbs_lin<0) = 0;
+cam_rgbs_lin(cam_rgbs_lin>1) = 1;
+
+% Fetch gray values of linearized cam RGBs
+cam_rgbs_gray_lin = fliplr(cam_rgbs_lin(:,19:24));
+
+clf;
+hold on;
+plot(munki_gray_Ys, cam_rgbs_gray_lin(1,:));
+plot(munki_gray_Ys, cam_rgbs_gray_lin(2,:));
+plot(munki_gray_Ys, cam_rgbs_gray_lin(3,:));
+title('linearized greyscale Y to RGB relationship');
+xlabel('munki gray Ys');
+ylabel('linearized camera grey RGBs');
+hold off;
+
+%% Camera RGB Visualization
+
+% visualize the original camera RGBs
+pix = permute(cam_RGBs, [3 2 1]);
+pix = reshape(pix, [6 4 3]);
+pix = imrotate(pix, -90);
+pix = flipdim(pix,2);
+figure;
+image(pix);
+title('original camera patch RGBs');
+
+% visualize the linearized camera RGBs
+pix = permute(cam_rgbs_lin, [3 2 1]);
+pix = reshape(pix, [6 4 3]);
+pix = imrotate(pix, -90);
+pix = flipdim(pix,2);
+figure;
+image(pix);
+title('linearized camera patch RGBs');
+
+%% Derive 3x3 Transformation Matrix
+
+% use the linearized camera rgbs and munki-measured XYZs
+% to derive an rgb to XYZ transformation matrix
+% pinv finds the matrix that provides the a least squares
+% linear solution for relating the rgbs and XYZs
+munki_data = importdata('munki_CC_XYZs_Labs.txt');
+munki_XYZs = munki_data(:,2:4)';
+cam_matrix = munki_XYZs * pinv(cam_rgbs_lin);
+
+fprintf('cam_matrix = \n');
+disp(cam_matrix);
+
+% estimate the CC XYZs from the linearized camera rgbs using
+% the camera model matrix
+cam_XYZs = cam_matrix * cam_rgbs_lin;
+
+fprintf('cam_XYZs = \n');
+disp(cam_XYZs);
+
+
+
+
+
