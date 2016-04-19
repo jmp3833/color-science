c2892dcfe191a95457d12166b4186dee8b531cf0
jmp3833 <jmp3833@rit.edu>
2016-04-19 16:14:12 -0400

    Calc and plot all lookup tables

--- project6/disp_char.m
+++ project6/disp_char.m
@@ -8,7 +8,7 @@ run load_ramps_data_1516;
 % Fetch largest XYZs for R G and B channels
 MAX_XYZS = [ramp_R_XYZs(11,:);ramp_G_XYZs(11,:);ramp_B_XYZs(11,:)];
 
-m_fwd = derive_fwd_matrix(MAX_XYZS, black_XYZ, white_XYZ);
+M_fwd = derive_fwd_matrix(MAX_XYZS, black_XYZ, white_XYZ);
 
 %% Derrive forward LUTs
 
@@ -23,7 +23,7 @@ red_sub_black(red_sub_black<0) = 0;
 red_sub_black(red_sub_black>1) = 1;
 
 %Multiply by inverse of first 3x3 of forward model
-fwd_inv_three = m_fwd(:,1:3)';
+fwd_inv_three = inv(M_fwd(:,1:3));
 est_RGB_radiometric_sclr = (red_sub_black * fwd_inv_three)';
 
 ramp_R_RS = est_RGB_radiometric_sclr(1,:);
@@ -46,7 +46,7 @@ blue_sub_black(blue_sub_black<0) = 0;
 blue_sub_black(blue_sub_black>1) = 1;
 
 %Multiply by inverse of first 3x3 of forward model
-fwd_inv_three = m_fwd(:,1:3)';
+fwd_inv_three = inv(M_fwd(:,1:3));
 est_RGB_radiometric_sclr_B = (blue_sub_black * fwd_inv_three)';
 
 ramp_B_BS = est_RGB_radiometric_sclr_B(3,:);
@@ -54,12 +54,12 @@ ramp_B_BS = est_RGB_radiometric_sclr_B(3,:);
 % define the 0-255 display values (digital counts) that correspond to the ramp values
 ramp_DCs_B = round(linspace(0,255,11));
 % interpolate the radiometric scalars across the full digital count range to form the forward LUTs
-RLUT_fwd_B = interp1(ramp_DCs,ramp_B_BS(1,:),[0:1:255],'spline');
+BLUT_fwd = interp1(ramp_DCs,ramp_B_BS(1,:),[0:1:255],'spline');
 
 %% Green Channel Forward LUTs
 
 % Subtract black from red ramp XYZ values
-green_sub_black = ramp_R_XYZs - repmat(black_XYZ,11,1);
+green_sub_black = ramp_G_XYZs - repmat(black_XYZ,11,1);
 
 %Normalize values by display white
 green_sub_black = green_sub_black ./ repmat(white_XYZ,11,1); 
@@ -69,7 +69,7 @@ green_sub_black(green_sub_black<0) = 0;
 green_sub_black(green_sub_black>1) = 1;
 
 %Multiply by inverse of first 3x3 of forward model
-fwd_inv_three = m_fwd(:,1:3)';
+fwd_inv_three = inv(M_fwd(:,1:3));
 est_RGB_radiometric_sclr_G = (green_sub_black * fwd_inv_three)';
 
 ramp_G_GS = est_RGB_radiometric_sclr_G(2,:);
@@ -77,10 +77,13 @@ ramp_G_GS = est_RGB_radiometric_sclr_G(2,:);
 % define the 0-255 display values (digital counts) that correspond to the ramp values
 ramp_DCs_G = round(linspace(0,255,11));
 % interpolate the radiometric scalars across the full digital count range to form the forward LUTs
-RLUT_fwd_G = interp1(ramp_DCs_G,ramp_G_GS(1,:),[0:1:255],'spline');
+GLUT_fwd = interp1(ramp_DCs_G,ramp_G_GS(1,:),[0:1:255],'spline');
 
 %% Plot all LUTs
 
 hold on;
-plot(0:255,RLUT_fwd,0:255,RLUT_fwd_G,0:255,RLUT_fwd_B)
+plot(0:255,RLUT_fwd, 0:255, GLUT_fwd, 0:255, BLUT_fwd);
 axis([0,255,0,1]);
+
+%% Test quality of forward model
+run test_forward_model_1516;
